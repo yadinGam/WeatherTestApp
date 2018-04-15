0b5db62d89a91eec8c3064180be7094687bc1f12
yadin <yadingaml@gmail.com>
2018-04-15 03:15:52 -0700

    1

---
+++ WeatherApp/CityWeatherCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CityWeatherCollectionViewCell.swift
+//  WeatherApp
+//
+//  Created by ידין גמליאל on 15/04/2018.
+//  Copyright © 2018 ידין גמליאל. All rights reserved.
+//
+
+import UIKit
+
+class CityWeatherCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var tempertureLabel: UILabel!
+    @IBOutlet weak var wetherDescriptionLabel: UILabel!
+    @IBOutlet weak var wetherImageView: UIImageView!
+    
+    
+}
--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -8,11 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , UICollectionViewDataSource, UICollectionViewDelegate{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // get a reference to our storyboard cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cityCell", for: indexPath as IndexPath) as! CityWeatherCollectionViewCell
+        
+        // Use the outlet in our custom class to get a reference to the UILabel in the cell
+        cell.nameLabel.text = String("\(indexPath.row)")
+        cell.backgroundColor = UIColor.cyan // make cell more visible in our example project
+        
+        
+        return cell
+    }
+    
+    @IBOutlet weak var weatherCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        weatherCollectionView.delegate = self
+        weatherCollectionView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
