f5a06d1f6e8c1d185cdd069d62435fb4da629ea1
yadin <yadingaml@gmail.com>
2018-04-15 05:35:29 -0700

    4

--- WeatherApp/CityJSONData.swift
+++ WeatherApp/CityJSONData.swift
@@ -26,32 +26,57 @@ struct CityJSONData: Codable{
     
     
     init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: JSONDataCodingKeys.self)
-        coord = try container.decode (Bool.self, forKey: .status)
-        weather = try container.decode (CampaignList.self, forKey: .data)
-        base = try container.decode (Bool.self, forKey: .status)
-        main = try container.decode (CampaignList.self, forKey: .data)
-        visibility = try container.decode (Bool.self, forKey: .status)
-        wind = try container.decode (CampaignList.self, forKey: .data)
-        clouds = try container.decode (Bool.self, forKey: .status)
-        dt = try container.decode (CampaignList.self, forKey: .data)
-        sys = try container.decode (Bool.self, forKey: .status)
-        id = try container.decode (CampaignList.self, forKey: .data)
-        name = try container.decode (Bool.self, forKey: .status)
-        code = try container.decode (CampaignList.self, forKey: .data)
+        let container = try decoder.container(keyedBy: CityJSONDataCodingKeys.self)
+        coord = try container.decode (Coordinate.self, forKey: .coord)
+        weather = try container.decode ([Weather].self, forKey: .weather)
+        base = try container.decode (String.self, forKey: .base)
+        main = try container.decode (MainData.self, forKey: .main)
+        visibility = try container.decode (Int.self, forKey: .visibility)
+        wind = try container.decode (Wind.self, forKey: .wind)
+        clouds = try container.decode (Clouds.self, forKey: .clouds)
+        dt = try container.decode (Int.self, forKey: .dt)
+        sys = try container.decode (Sys.self, forKey: .sys)
+        id = try container.decode (Int.self, forKey: .id)
+        name = try container.decode (String.self, forKey: .name)
+        cod = try container.decode (Int.self, forKey: .cod)
         
     }
-    //JSONData struct
+    
     func encode(to encoder: Encoder) throws {
-        var container = encoder.container (keyedBy: JSONDataCodingKeys.self)
-        try container.encode (status, forKey: .status)
-        try container.encode (data, forKey: .data)
+        var container = encoder.container (keyedBy: CityJSONDataCodingKeys.self)
+        try container.encode (coord, forKey: .coord)
+        try container.encode (weather, forKey: .weather)
+        try container.encode (base, forKey: .base)
+        try container.encode (main, forKey: .main)
+        try container.encode (visibility, forKey: .visibility)
+        try container.encode (wind, forKey: .wind)
+        try container.encode (clouds, forKey: .clouds)
+        try container.encode (dt, forKey: .dt)
+        try container.encode (sys, forKey: .sys)
+        try container.encode (id, forKey: .id)
+             try container.encode (name, forKey: .name)
+             try container.encode (cod, forKey: .cod)
+        
     }
+    
+    
     
     struct Coordinate: Codable {
         let lon: Double
         let lat: Double
         
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CoordinateCodingKeys.self)
+            lon = try container.decode (Double.self, forKey: .lon)
+            lat = try container.decode (Double.self, forKey: .lat)
+
+        }
+        func encode(to encoder: Encoder) throws {
+            var container = encoder.container (keyedBy: CoordinateCodingKeys.self)
+            try container.encode (lon, forKey: .lon)
+            try container.encode (lat, forKey: .lat)
+            
+        }
         
         enum CoordinateCodingKeys: String, CodingKey {
             case lon
@@ -66,6 +91,28 @@ struct CityJSONData: Codable{
             let main:String
             let description:String
             let icon: String
+        
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: WeatherCodingKeys.self)
+            id = try container.decode (Int.self, forKey: .id)
+            
+            main = try container.decode (String.self, forKey: .id)
+            
+            description = try container.decode (String.self, forKey: .id)
+            
+            icon = try container.decode (String.self, forKey: .id)
+            
+        }
+ 
+        func encode(to encoder: Encoder) throws {
+            var container = encoder.container (keyedBy: WeatherCodingKeys.self)
+            
+            try container.encode (id, forKey: .id)
+            try container.encode (main, forKey: .main)
+            try container.encode (description, forKey: .description)
+            try container.encode (icon, forKey: .icon)
+            
+        }
         
         enum WeatherCodingKeys: String, CodingKey {
             case id
@@ -84,6 +131,25 @@ struct CityJSONData: Codable{
         let temp_min: Double
         let temp_max: Double
         
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: MainDataCodingKeys.self)
+            temp = try container.decode (Double.self, forKey: .temp)
+            pressure = try container.decode (Int.self, forKey: .pressure)
+            humidity = try container.decode (Int.self, forKey: .humidity)
+            temp_min = try container.decode (Double.self, forKey: .temp_min)
+            temp_max = try container.decode (Double.self, forKey: .temp_max)
+        }
+        //JSONData struct
+        func encode(to encoder: Encoder) throws {
+            var container = encoder.container (keyedBy: MainDataCodingKeys.self)
+            
+            try container.encode (temp, forKey: .temp)
+            try container.encode (pressure, forKey: .pressure)
+            try container.encode (humidity, forKey: .humidity)
+            try container.encode (temp_min, forKey: .temp_min)
+            try container.encode (temp_max, forKey: .temp_max)
+        }
+        
         enum MainDataCodingKeys: String, CodingKey {
             case temp
             case pressure
@@ -100,6 +166,19 @@ struct CityJSONData: Codable{
         let speed:Double
         let deg:Int
     
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: WindDataCodingKeys.self)
+            speed = try container.decode (Double.self, forKey: .speed)
+            deg = try container.decode (Int.self, forKey: .deg)
+            
+        }
+        //JSONData struct
+        func encode(to encoder: Encoder) throws {
+            var container = encoder.container (keyedBy: WindDataCodingKeys.self)
+            try container.encode (speed, forKey: .speed)
+            try container.encode (deg, forKey: .deg)
+            
+        }
         
         enum WindDataCodingKeys: String, CodingKey {
             case speed
@@ -116,6 +195,19 @@ struct CityJSONData: Codable{
         
         let all:Int
         
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CloudsDataCodingKeys.self)
+            all = try container.decode (Int.self, forKey: .all)
+            
+        }
+        //JSONData struct
+        func encode(to encoder: Encoder) throws {
+            var container = encoder.container (keyedBy: CloudsDataCodingKeys.self)
+            
+            try container.encode (all, forKey: .all)
+            
+        }
+        
         enum CloudsDataCodingKeys: String, CodingKey {
             case all
         }
@@ -130,6 +222,30 @@ struct CityJSONData: Codable{
          let country:String
          let sunrise:Int
          let sunset:Int
+        
+        
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: SysDataCodingKeys.self)
+            type = try container.decode (Int.self, forKey: .type)
+            id = try container.decode (Int.self, forKey: .id)
+            message = try container.decode (Double.self, forKey: .message)
+            country = try container.decode (String.self, forKey: .country)
+            sunrise = try container.decode (Int.self, forKey: .sunrise)
+            sunset = try container.decode (Int.self, forKey: .sunset)
+        }
+        //JSONData struct
+        func encode(to encoder: Encoder) throws {
+            var container = encoder.container (keyedBy: SysDataCodingKeys.self)
+            
+            try container.encode (type, forKey: .type)
+            try container.encode (id, forKey: .id)
+            try container.encode (message, forKey: .message)
+            try container.encode (country, forKey: .country)
+            try container.encode (sunrise, forKey: .sunrise)
+            try container.encode (sunset, forKey: .sunset)
+            
+            
+        }
         
         enum SysDataCodingKeys: String, CodingKey {
             case type
