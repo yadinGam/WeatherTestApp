ca53caf6afdec3518c5961ef44e2220542bc5b77
yadin <yadingaml@gmail.com>
2018-04-15 03:39:26 -0700

    2

---
+++ WeatherApp/City.swift
@@ -0,0 +1,14 @@
+//
+//  City.swift
+//  WeatherApp
+//
+//  Created by ידין גמליאל on 15/04/2018.
+//  Copyright © 2018 ידין גמליאל. All rights reserved.
+//
+
+import Foundation
+
+struct City{
+    let lat: Double!
+    let lon: Double!
+}
--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -9,8 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController , UICollectionViewDataSource, UICollectionViewDelegate{
+    
+   
+    
+
+    
+    @IBOutlet weak var weatherCollectionView: UICollectionView!
+    
+     private var cities:[City] = [City]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        weatherCollectionView.delegate = self
+        weatherCollectionView.dataSource = self
+        
+        
+        let city0: City = City(lat: 3.44444,lon: 4.555555)
+        let city1: City = City(lat: 3.44444,lon: 4.555555)
+        let city2: City = City(lat: 3.44444,lon: 4.555555)
+        let city3: City = City(lat: 3.44444,lon: 4.555555)
+        let city4: City = City(lat: 3.44444,lon: 4.555555)
+         let city5: City =  City(lat: 3.44444,lon: 4.555555)
+         let city6: City =  City(lat: 3.44444,lon: 4.555555)
+        let city7: City =  City(lat: 3.44444,lon: 4.555555)
+        let city8: City =  City(lat: 3.44444,lon: 4.555555)
+        let city9: City = City(lat: 3.44444,lon: 4.555555)
+        
+       
+        cities.append(city0)
+        cities.append(city1)
+        cities.append(city2)
+        cities.append(city3)
+        cities.append(city4)
+        cities.append(city5)
+        cities.append(city6)
+        cities.append(city7)
+        cities.append(city8)
+        cities.append(city9)
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return cities.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -23,20 +70,6 @@ class ViewController: UIViewController , UICollectionViewDataSource, UICollectio
         
         
         return cell
-    }
-    
-    @IBOutlet weak var weatherCollectionView: UICollectionView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        weatherCollectionView.delegate = self
-        weatherCollectionView.dataSource = self
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
